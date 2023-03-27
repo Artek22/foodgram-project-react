ada7257bc420a4e91b7750b00014e557820ca192
Андрей <temp60@mail.ru>
2023-03-27 16:29:07 +0300

    admin.py

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.admin import display
 
-# Register your models here.
+from .models import (Ingredient, IngredientRecipe, Recipe, Tag)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'author', 'added_in_favorites')
+    readonly_fields = ('added_in_favorites',)
+    list_filter = ('author', 'name', 'tags',)
+
+    @display(description='Количество в избранных')
+    def added_in_favorites(self, obj):
+        return obj.favorites.count()
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit',)
+    list_filter = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug',)
+
+
+@admin.register(IngredientRecipe)
+class IngredientRecipe(admin.ModelAdmin):
+    list_display = ('recipe_id', 'ingredient_id', 'amount',)
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,13 +1,92 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, RegexValidator
 
-
-class Recipe(models.Model):
-    pass
+User = get_user_model()
 
 
 class Tag(models.Model):
-    pass
+    name = models.CharField(max_length=128, verbose_name='Название', unique=True)
+    color = models.CharField(
+        max_length=7,
+        verbose_name='HEX-код',
+        validators=[
+            RegexValidator(
+                regex='^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$',
+                message='Формат HEX-кода введен неправильно!',
+            )
+        ]
+    )
+    slug = models.SlugField(max_length=128, verbose_name='Слаг', unique=True)
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
 
 
 class Ingredient(models.Model):
-    pass
+    name = models.CharField(max_length=200, verbose_name='Название', null=True)
+    measurement_unit = models.CharField(
+        max_length=128,
+        verbose_name='Единица измерения',
+        default='г',
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор',
+    )
+    name = models.CharField(max_length=200, verbose_name='Название')
+    image = models.ImageField(
+        verbose_name='Изображение',
+        upload_to='recipes/',
+    )
+    text = models.TextField(verbose_name='Описание')
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='IngredientRecipe',
+        related_name='recipes',
+        verbose_name='Ингредиенты',
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Время приготовления',
+        validators=[MinValueValidator(1, message='Минимальное значение: 1')],
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='recipes',
+        verbose_name='Теги',
+    )
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+class IngredientRecipe(models.Model):
+    ingredient_id = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        verbose_name='Ингредиент',
+    )
+    recipe_id = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='ingredient_list',
+        verbose_name='Рецепт',
+    )
+    amount = models.PositiveSmallIntegerField(
+        verbose_name='Количество',
+    )
