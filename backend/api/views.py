ca3b1015f87934122a1268d511f254c2186dc10f
Андрей <temp60@mail.ru>
2023-03-28 20:04:33 +0300

    Pagination

--- backend/api/views.py
+++ backend/api/views.py
@@ -12,7 +12,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-# from .filters import IngredientFilter, RecipeFilter
+from .filters import IngredientFilter, RecipeFilter
 # from .pagination import CustomPagination
 # from .permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly
 from .serializers import (IngredientSerializer, RecipeReadSerializer,
@@ -22,15 +22,15 @@ from .serializers import (IngredientSerializer, RecipeReadSerializer,
 
 class IngredientViewSet(ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
-    # serializer_class = IngredientSerializer
+    serializer_class = IngredientSerializer
     # permission_classes = (IsAdminOrReadOnly,)
     filter_backends = (DjangoFilterBackend,)
-    # filterset_class = IngredientFilter
+    filterset_class = IngredientFilter
 
 
 class TagViewSet(ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
-    # serializer_class = TagSerializer
+    serializer_class = TagSerializer
     # permission_classes = (IsAdminOrReadOnly,)
 
 
@@ -39,15 +39,15 @@ class RecipeViewSet(ModelViewSet):
     # permission_classes = (IsAuthorOrReadOnly | IsAdminOrReadOnly,)
     # pagination_class = CustomPagination
     filter_backends = (DjangoFilterBackend,)
-    # filterset_class = RecipeFilter
+    filterset_class = RecipeFilter
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    # def get_serializer_class(self):
-    #     if self.request.method in SAFE_METHODS:
-    #         return RecipeReadSerializer
-    #     return RecipeWriteSerializer
+    def get_serializer_class(self):
+        if self.request.method in SAFE_METHODS:
+            return RecipeReadSerializer
+        return RecipeWriteSerializer
 
     # @action(
     #     detail=True,
@@ -70,24 +70,24 @@ class RecipeViewSet(ModelViewSet):
     #         return self.add_to(ShoppingCart, request.user, pk)
     #     else:
     #         return self.delete_from(ShoppingCart, request.user, pk)
-
-    def add_to(self, model, user, pk):
-        if model.objects.filter(user=user, recipe__id=pk).exists():
-            return Response({'errors': 'Рецепт уже добавлен!'},
-                            status=status.HTTP_400_BAD_REQUEST)
-        recipe = get_object_or_404(Recipe, id=pk)
-        model.objects.create(user=user, recipe=recipe)
-        # serializer = RecipeShortSerializer(recipe)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def delete_from(self, model, user, pk):
-        obj = model.objects.filter(user=user, recipe__id=pk)
-        if obj.exists():
-            obj.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response({'errors': 'Рецепт уже удален!'},
-                        status=status.HTTP_400_BAD_REQUEST)
-
+    #
+    # def add_to(self, model, user, pk):
+    #     if model.objects.filter(user=user, recipe__id=pk).exists():
+    #         return Response({'errors': 'Рецепт уже добавлен!'},
+    #                         status=status.HTTP_400_BAD_REQUEST)
+    #     recipe = get_object_or_404(Recipe, id=pk)
+    #     model.objects.create(user=user, recipe=recipe)
+    #     # serializer = RecipeShortSerializer(recipe)
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #
+    # def delete_from(self, model, user, pk):
+    #     obj = model.objects.filter(user=user, recipe__id=pk)
+    #     if obj.exists():
+    #         obj.delete()
+    #         return Response(status=status.HTTP_204_NO_CONTENT)
+    #     return Response({'errors': 'Рецепт уже удален!'},
+    #                     status=status.HTTP_400_BAD_REQUEST)
+    #
     # @action(
     #     detail=False,
     #     permission_classes=[IsAuthenticated]
