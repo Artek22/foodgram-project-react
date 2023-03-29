ba3a25f7eef77f0de4a60f5283b5ce24afbb3fe0
Андрей <temp60@mail.ru>
2023-03-29 13:02:45 +0300

    api views, urls

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,124 +1,166 @@
-from datetime import datetime
-
 from django.db.models import Sum
-from django.http import HttpResponse
+from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from recipes.models import (Ingredient, IngredientRecipe, Recipe, Tag)
-from rest_framework import status
+from djoser.views import UserViewSet
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
+from rest_framework.permissions import (IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+
+from recipes.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
+                            ShoppingCart, Tag)
+from users.models import Follow, User
 
 from .filters import IngredientFilter, RecipeFilter
-# from .pagination import CustomPagination
-# from .permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly
-from .serializers import (IngredientSerializer, RecipeReadSerializer,
-                          RecipeShortSerializer, RecipeWriteSerializer,
-                          TagSerializer)
+from .pagination import CustomPagination
+from .permissions import AuthorPermission
+from .serializers import (CreateRecipeSerializer, FavoriteSerializer,
+                          IngredientSerializer, RecipeReadSerializer,
+                          ShoppingCartSerializer, SubscribeListSerializer,
+                          TagSerializer, UserSerializer)
 
 
-class IngredientViewSet(ReadOnlyModelViewSet):
-    queryset = Ingredient.objects.all()
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    """ Вывод ингредиентов """
     serializer_class = IngredientSerializer
-    # permission_classes = (IsAdminOrReadOnly,)
-    filter_backends = (DjangoFilterBackend,)
-    filterset_class = IngredientFilter
+    queryset = Ingredient.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+    filter_backends = (IngredientFilter, )
+    search_fields = ('^name', )
+    pagination_class = None
 
 
-class TagViewSet(ReadOnlyModelViewSet):
+class TagViewSet(viewsets.ModelViewSet):
+    """ Вывод тегов """
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-    # permission_classes = (IsAdminOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+    pagination_class = None
 
 
-class RecipeViewSet(ModelViewSet):
+class RecipeViewSet(viewsets.ModelViewSet):
+    """ Вывод работы с рецептами """
     queryset = Recipe.objects.all()
-    # permission_classes = (IsAuthorOrReadOnly | IsAdminOrReadOnly,)
-    # pagination_class = CustomPagination
-    filter_backends = (DjangoFilterBackend,)
+    serializer_class = CreateRecipeSerializer
+    permission_classes = (AuthorPermission, )
+    pagination_class = CustomPagination
+    filter_backends = (DjangoFilterBackend, )
     filterset_class = RecipeFilter
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
     def get_serializer_class(self):
-        if self.request.method in SAFE_METHODS:
+        if self.request.method == 'GET':
             return RecipeReadSerializer
-        return RecipeWriteSerializer
+        return CreateRecipeSerializer
 
-    # @action(
-    #     detail=True,
-    #     methods=['post', 'delete'],
-    #     permission_classes=[IsAuthenticated]
-    # )
-    # def favorite(self, request, pk):
-    #     if request.method == 'POST':
-    #         return self.add_to(Favourite, request.user, pk)
-    #     else:
-    #         return self.delete_from(Favourite, request.user, pk)
+    @staticmethod
+    def send_message(ingredients):
+        shopping_list = 'Купить в магазине:'
+        for ingredient in ingredients:
+            shopping_list += (
+                f"\n{ingredient['ingredient__name']} "
+                f"({ingredient['ingredient__measurement_unit']}) - "
+                f"{ingredient['amount']}")
+        file = 'shopping_list.txt'
+        response = HttpResponse(shopping_list, content_type='text/plain')
+        response['Content-Disposition'] = f'attachment; filename="{file}.txt"'
+        return response
 
-    # @action(
-    #     detail=True,
-    #     methods=['post', 'delete'],
-    #     permission_classes=[IsAuthenticated]
-    # )
-    # def shopping_cart(self, request, pk):
-    #     if request.method == 'POST':
-    #         return self.add_to(ShoppingCart, request.user, pk)
-    #     else:
-    #         return self.delete_from(ShoppingCart, request.user, pk)
-    #
-    # def add_to(self, model, user, pk):
-    #     if model.objects.filter(user=user, recipe__id=pk).exists():
-    #         return Response({'errors': 'Рецепт уже добавлен!'},
-    #                         status=status.HTTP_400_BAD_REQUEST)
-    #     recipe = get_object_or_404(Recipe, id=pk)
-    #     model.objects.create(user=user, recipe=recipe)
-    #     # serializer = RecipeShortSerializer(recipe)
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #
-    # def delete_from(self, model, user, pk):
-    #     obj = model.objects.filter(user=user, recipe__id=pk)
-    #     if obj.exists():
-    #         obj.delete()
-    #         return Response(status=status.HTTP_204_NO_CONTENT)
-    #     return Response({'errors': 'Рецепт уже удален!'},
-    #                     status=status.HTTP_400_BAD_REQUEST)
-    #
-    # @action(
-    #     detail=False,
-    #     permission_classes=[IsAuthenticated]
-    # )
-    # def download_shopping_cart(self, request):
-    #     user = request.user
-    #     if not user.shopping_cart.exists():
-    #         return Response(status=HTTP_400_BAD_REQUEST)
-    #
-    #     ingredients = IngredientInRecipe.objects.filter(
-    #         recipe__shopping_cart__user=request.user
-    #     ).values(
-    #         'ingredient__name',
-    #         'ingredient__measurement_unit'
-    #     ).annotate(amount=Sum('amount'))
-    #
-    #     today = datetime.today()
-    #     shopping_list = (
-    #         f'Список покупок для: {user.get_full_name()}\n\n'
-    #         f'Дата: {today:%Y-%m-%d}\n\n'
-    #     )
-    #     shopping_list += '\n'.join([
-    #         f'- {ingredient["ingredient__name"]} '
-    #         f'({ingredient["ingredient__measurement_unit"]})'
-    #         f' - {ingredient["amount"]}'
-    #         for ingredient in ingredients
-    #     ])
-    #     shopping_list += f'\n\nFoodgram ({today:%Y})'
-    #
-    #     filename = f'{user.username}_shopping_list.txt'
-    #     response = HttpResponse(shopping_list, content_type='text/plain')
-    #     response['Content-Disposition'] = f'attachment; filename={filename}'
-    #
-    #     return response
+    @action(detail=False, methods=['GET'])
+    def download_shopping_cart(self, request):
+        ingredients = IngredientRecipe.objects.filter(
+            recipe__shopping_list__user=request.user
+        ).order_by('ingredient__name').values(
+            'ingredient__name', 'ingredient__measurement_unit'
+        ).annotate(amount=Sum('amount'))
+        return self.send_message(ingredients)
+
+    @action(
+        detail=True,
+        methods=('POST',),
+        permission_classes=[IsAuthenticated])
+    def shopping_cart(self, request, pk):
+        context = {'request': request}
+        recipe = get_object_or_404(Recipe, id=pk)
+        data = {
+            'user': request.user.id,
+            'recipe': recipe.id
+        }
+        serializer = ShoppingCartSerializer(data=data, context=context)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @shopping_cart.mapping.delete
+    def destroy_shopping_cart(self, request, pk):
+        get_object_or_404(
+            ShoppingCart,
+            user=request.user.id,
+            recipe=get_object_or_404(Recipe, id=pk)
+        ).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(
+        detail=True,
+        methods=('POST',),
+        permission_classes=[IsAuthenticated])
+    def favorite(self, request, pk):
+        context = {"request": request}
+        recipe = get_object_or_404(Recipe, id=pk)
+        data = {
+            'user': request.user.id,
+            'recipe': recipe.id
+        }
+        serializer = FavoriteSerializer(data=data, context=context)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @favorite.mapping.delete
+    def destroy_favorite(self, request, pk):
+        get_object_or_404(
+            Favorite,
+            user=request.user,
+            recipe=get_object_or_404(Recipe, id=pk)
+        ).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserViewSet(UserViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    pagination_class = CustomPagination
+
+    @action(
+        detail=True,
+        methods=['post', 'delete'],
+        permission_classes=[IsAuthenticated],
+    )
+    def subscribe(self, request, id):
+        user = request.user
+        author = get_object_or_404(User, pk=id)
+
+        if request.method == 'POST':
+            serializer = SubscribeListSerializer(
+                author, data=request.data, context={'request': request}
+            )
+            serializer.is_valid(raise_exception=True)
+            Follow.objects.create(user=user, author=author)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if request.method == 'DELETE':
+            get_object_or_404(
+                Follow, user=user, author=author
+            ).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=False, permission_classes=[IsAuthenticated])
+    def subscriptions(self, request):
+        user = request.user
+        queryset = User.objects.filter(following__user=user)
+        pages = self.paginate_queryset(queryset)
+        serializer = SubscribeListSerializer(
+            pages, many=True, context={'request': request}
+        )
+        return self.get_paginated_response(serializer.data)
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,73 +1,115 @@
+from colorfield.fields import ColorField
+from django.conf import settings
+from django.core.validators import (MaxValueValidator, MinValueValidator,
+                                    RegexValidator)
 from django.db import models
-from django.contrib.auth import get_user_model
-from django.core.validators import MinValueValidator, RegexValidator
+from django.db.models import UniqueConstraint
 
-User = get_user_model()
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=128, verbose_name='Тег', unique=True)
-    color = models.CharField(
-        max_length=7,
-        verbose_name='Цвет',
-        validators=[
-            RegexValidator(
-                regex='^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$',
-                message='Формат HEX-кода введен неправильно!',
-            )
-        ]
-    )
-    slug = models.SlugField(max_length=128, verbose_name='Слаг', unique=True)
-
-    class Meta:
-        verbose_name = 'Тег'
-        verbose_name_plural = 'Теги'
+from users.models import User
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=200, verbose_name='Название', null=True)
+    """ Модель ингридиентов. """
+    name = models.CharField(
+        max_length=settings.LENGTH_OF_FIELDS_RECIPES,
+        verbose_name='Название ингридиента',
+        db_index=True
+    )
     measurement_unit = models.CharField(
-        max_length=128,
-        verbose_name='Единица измерения',
-        default='г',
+        max_length=settings.LENGTH_OF_FIELDS_RECIPES,
+        verbose_name='Еденицы измерения'
+    )
+
+    class Meta():
+        verbose_name = 'Ингридиенты'
+        verbose_name_plural = 'Ингридиенты'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'measurement_unit'],
+                name='unique_name_measurement_unit'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.name}, {self.measurement_unit}'
+
+
+class Tag(models.Model):
+    """ Модель тегов."""
+    name = models.CharField(
+        verbose_name='Название тега',
+        max_length=settings.LENGTH_OF_FIELDS_RECIPES,
+        db_index=True,
+        unique=True
+    )
+    color = ColorField(
+        verbose_name='HEX-код',
+        format='hex',
+        max_length=7,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex="^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$",
+                message='Проверьте вводимый формат',
+            )
+        ],
+    )
+    slug = models.SlugField(
+        max_length=settings.LENGTH_OF_FIELDS_RECIPES,
+        verbose_name='Slug',
+        unique=True
     )
 
     class Meta:
-        verbose_name = 'Ингредиент'
-        verbose_name_plural = 'Ингредиенты'
+        ordering = ('name',)
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
+    """ Модель рецептов. """
     author = models.ForeignKey(
         User,
+        verbose_name='Автор рецепта',
         on_delete=models.CASCADE,
-        related_name='recipes',
-        verbose_name='Автор',
+        related_name='recipes'
     )
-    name = models.CharField(max_length=200, verbose_name='Название')
+    name = models.CharField(
+        verbose_name='Название рецепта',
+        max_length=settings.LENGTH_OF_FIELDS_RECIPES,
+    )
     image = models.ImageField(
-        verbose_name='Изображение',
-        upload_to='recipes/',
+        upload_to='recipes/image/',
+        verbose_name='Изображение'
     )
     text = models.TextField(verbose_name='Описание')
     ingredients = models.ManyToManyField(
         Ingredient,
-        through='IngredientRecipe',
-        related_name='recipes',
-        verbose_name='Ингредиенты',
-    )
-    cooking_time = models.PositiveSmallIntegerField(
-        verbose_name='Время приготовления',
-        validators=[MinValueValidator(1, message='Минимальное значение: 1')],
+        verbose_name='Ингридиенты',
+        through='IngredientRecipe'
     )
     tags = models.ManyToManyField(
         Tag,
-        related_name='recipes',
-        verbose_name='Теги',
+        verbose_name='Теги'
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Время готовки',
+        validators=[MinValueValidator(
+            1, message='Время приготовления не менее 1 минуты!'
+        ), MaxValueValidator(
+            1441, message='Время приготовления не более 24 часов!'
+        )]
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True
     )
 
     class Meta:
-        ordering = ['-id']
+        ordering = ('-pub_date',)
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
 
@@ -75,18 +117,76 @@ class Recipe(models.Model):
         return self.name
 
 
-class IngredientRecipe(models.Model):
-    ingredient_id = models.ForeignKey(
-        Ingredient,
+class FavoriteShoppingCart(models.Model):
+    """ Связывающая модель списка покупок и избранного. """
+    user = models.ForeignKey(
+        User,
         on_delete=models.CASCADE,
-        verbose_name='Ингредиент',
+        verbose_name='Пользователь',
+
     )
-    recipe_id = models.ForeignKey(
+    recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='ingredient_list',
         verbose_name='Рецепт',
     )
-    amount = models.PositiveSmallIntegerField(
-        verbose_name='Количество',
+
+    class Meta:
+        abstract = True
+        constraints = [
+            UniqueConstraint(
+                fields=('user', 'recipe'),
+                name='%(app_label)s_%(class)s_unique'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user} :: {self.recipe}'
+
+
+class Favorite(FavoriteShoppingCart):
+    """ Модель добавление в избраное. """
+
+    class Meta(FavoriteShoppingCart.Meta):
+        default_related_name = 'favorites'
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранное'
+
+
+class ShoppingCart(FavoriteShoppingCart):
+    """ Модель списка покупок. """
+
+    class Meta(FavoriteShoppingCart.Meta):
+        default_related_name = 'shopping_list'
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзина'
+
+
+class IngredientRecipe(models.Model):
+    """ Ингридиенты рецепта. """
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        verbose_name='Ингредиент'
     )
+    recipe = models.ForeignKey(
+        Recipe,
+        verbose_name='Рецепт',
+        on_delete=models.CASCADE,
+        related_name='ingredienttorecipe'
+    )
+    amount = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1)],
+        verbose_name='Количество ингредиента'
+    )
+
+    class Meta:
+        ordering = ('-id', )
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты рецепта'
+
+    def __str__(self):
+        return (
+            f'{self.ingredient.name} :: {self.ingredient.measurement_unit}'
+            f' - {self.amount} '
+        )
