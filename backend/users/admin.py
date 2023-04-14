fa93d4d1cb1bdb46b4c483dc3446160400b21eea
Андрей <temp60@mail.ru>
2023-04-14 10:34:57 +0300

    user admin filters field changed

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -6,7 +6,7 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name')
     search_fields = ('username', 'email')
-    list_filter = ('first_name', 'last_name')
+    list_filter = ('first_name', 'email')
     ordering = ('username', )
     empty_value_display = '-пусто-'
 
